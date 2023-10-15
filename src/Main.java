879ec9ab71577dad96636d1452eb3aeb37e5543d
PawelSlowik <pawel.slowik@vp.pl>
2023-10-15 11:12:38 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-
         int liczba = 5;
         long silnia = obliczenieSilni(liczba);
         System.out.print("SIlnia z liczby" + liczba + "wynosi = " + silnia);
     }
-
-
-
         public static long obliczenieSilni(int n){
         if (n==0) {
             return 1;
         }
         else {
           return  n * obliczenieSilni (n-1);
-
         }
         }
 
